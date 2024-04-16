28acc381b3db6986bd1393af6168e438c2e3f72c
olgevd <165941281+olgevd@users.noreply.github.com>
2024-04-16 23:52:51 +0300

    откорректировано

--- Script-4.sql
+++ Script-4.sql
@@ -1,7 +1,73 @@
 DROP TABLE IF EXISTS tmp_sources;
 CREATE TEMP TABLE tmp_sources AS 
+SELECT  order_id,
+        order_created_date,
+        order_completion_date,
+        order_status,
+        craftsman_id,
+        craftsman_name,
+        craftsman_address,
+        craftsman_birthday,
+        craftsman_email,
+        product_id,
+        product_name,
+        product_description,
+        product_type,
+        product_price,
+        customer_id,
+        customer_name,
+        customer_address,
+        customer_birthday,
+        customer_email 
+  FROM source1.craft_market_wide
+UNION
+SELECT  t2.order_id,
+        t2.order_created_date,
+        t2.order_completion_date,
+        t2.order_status,
+        t1.craftsman_id,
+        t1.craftsman_name,
+        t1.craftsman_address,
+        t1.craftsman_birthday,
+        t1.craftsman_email,
+        t1.product_id,
+        t1.product_name,
+        t1.product_description,
+        t1.product_type,
+        t1.product_price,
+        t2.customer_id,
+        t2.customer_name,
+        t2.customer_address,
+        t2.customer_birthday,
+        t2.customer_email 
+  FROM source2.craft_market_masters_products t1 
+    JOIN source2.craft_market_orders_customers t2 ON t2.product_id = t1.product_id and t1.craftsman_id = t2.craftsman_id 
+UNION
+SELECT  t1.order_id,
+        t1.order_created_date,
+        t1.order_completion_date,
+        t1.order_status,
+        t2.craftsman_id,
+        t2.craftsman_name,
+        t2.craftsman_address,
+        t2.craftsman_birthday,
+        t2.craftsman_email,
+        t1.product_id,
+        t1.product_name,
+        t1.product_description,
+        t1.product_type,
+        t1.product_price,
+        t3.customer_id,
+        t3.customer_name,
+        t3.customer_address,
+        t3.customer_birthday,
+        t3.customer_email
+  FROM source3.craft_market_orders t1
+    JOIN source3.craft_market_craftsmans t2 ON t1.craftsman_id = t2.craftsman_id 
+    JOIN source3.craft_market_customers t3 ON t1.customer_id = t3.customer_id
+UNION
 SELECT  
-	cpo.id,
+	/*cpo.id,*/
 	cpo.craftsman_id,
 	cpo.craftsman_name,
 	cpo.craftsman_address,
